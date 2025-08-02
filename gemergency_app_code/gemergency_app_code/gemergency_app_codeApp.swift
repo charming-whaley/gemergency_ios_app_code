b6b000e509bdc9da18d9653cb9e6388e75ab1df9
Katkov Fedor <pweverj126@gmail.com>
2025-08-02 00:17:29 -0700

    f

---
+++ gemergency_app_code/gemergency_app_code/gemergency_app_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gemergency_app_codeApp.swift
+//  gemergency_app_code
+//
+//  Created by Fedya Katkov on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct gemergency_app_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
